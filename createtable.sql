2c7dd520fcbfd7e9f03723e10624e70684c665db
jefferson1590 <jeffersonti@outlook.com.br>
2016-12-08 13:49:56 -0200

    Update createtable.sql

--- createtable.sql
+++ createtable.sql
@@ -1,5 +1,5 @@
 DROP TABLE IF EXISTS tbcomando;
-DROP TABLE IF EXISTS tbbotresposta;
+DROP TABLE IF EXISTS tbbotresultado;
 	  create table tbcomando(
 id_comando int not null auto_increment,
 nm_comando varchar(255) not null,
@@ -7,11 +7,11 @@ primary key (id_comando),
 CONSTRAINT uk_tbcomando_nm_comando UNIQUE (nm_comando) );
 
 
-	  CREATE TABLE 'tbbotresposta' (
-id_resposta int NOT NULL AUTO_INCREMENT,
+	  CREATE TABLE 'tbbotresultado' (
+id_resultado int NOT NULL AUTO_INCREMENT,
 	id_comando INT NOT NULL,
 first_name varchar(255) NOT NULL,
-resposta varchar(255) DEFAULT NULL, update_id varchar(255) NOT NULL,
+resultado varchar(255) DEFAULT NULL, update_id varchar(255) NOT NULL,
 PRIMARY KEY (id_resposta),
-CONSTRAINT uk_tbresposta_update_id UNIQUE (update_id),
+CONSTRAINT uk_tbresultado_update_id UNIQUE (update_id),
 FOREIGN KEY (id_comando) references tbcomando(id_comando));
